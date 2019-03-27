bb684b22c1da69314ae0a995f106a283cba706e9
Lucas Vaz <lucastarraf@hotmail.com>
2019-03-26 22:42:46 -0300

    Gauss

--- mec.py
+++ mec.py
@@ -1,27 +1,40 @@
 import numpy as np
+import math as m
 
-dic={}
-f = open("data.txt", "r")
-auxiliar=""
-for x in f:
-    if(x[0]=="*"):
-        auxiliar=x.rstrip()
-        dic[auxiliar]=[]
-        counter=0
-    if(x[0]!='*'):
-        dic[auxiliar].append(x.rstrip())
+def readDic(filename):
+    dic={}
+    f = open(filename, "r")
+    auxiliar=""
+    for x in f:
+        if(x[0]=="*"):
+            auxiliar=x.rstrip()
+            dic[auxiliar]=[]
+            counter=0
+        if(x[0]!='*'):
+            dic[auxiliar].append(x.rstrip())
 
-for i in dic:
-    for j in range(len(dic[i])):
-        dic[i][j]=dic[i][j].split()
-        for k in range(len(dic[i][j])):
-            try:
-                dic[i][j][k]=float(dic[i][j][k])
-            except ValueError:
-                p=0;
+    for i in dic:
+        for j in range(len(dic[i])):
+            dic[i][j]=dic[i][j].split()
+            for k in range(len(dic[i][j])):
+                try:
+                    dic[i][j][k]=float(dic[i][j][k])
+                except ValueError:
+                    p=0;
 
-def calcMRigidez(E,A,l,ponto1,ponto2):
+def calcMRigidez(e,a,l,pontoA,pontoB):
+
+    alfa= m.atan2(pontoB[1]-pontoA[1],pontoB[0]-pontoA[0])
+    c=m.cos(alfa)
+    s=m.sin(alfa)
+
+    M = np.array([[c**2,c*s,-c**2,-c*s],
+    [c*s,s**2,-c*s,-s**2],
+    [-c**2,-c*s,c**2,c*s],
+    [-c*s,-s**2,c*s,s**2]])
 
 
+    M = M.round(7)
+    return ((e*a/l)*M)
 
-print(dic["*ELEMENT_GROUPS"][1][0])
+def Gauss(u, F, M, tolerancia, loopN)
