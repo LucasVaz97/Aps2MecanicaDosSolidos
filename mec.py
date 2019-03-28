642b0d52bb7fda42a8c4d105312400f19d2c6da8
TeraByte900 <fe.n.teracini@hotmail.com>
2019-03-28 13:38:57 -0300

    mec-gauss corrigido

--- mec.py
+++ mec.py
@@ -1,58 +1,62 @@
 import numpy as np
 import math as m
 
+
 def readDic(filename):
-    dic={}
+    dic = {}
     f = open(filename, "r")
-    auxiliar=""
+    auxiliar = ""
     for x in f:
-        if(x[0]=="*"):
-            auxiliar=x.rstrip()
-            dic[auxiliar]=[]
-            counter=0
-        if(x[0]!='*'):
+        if(x[0] == "*"):
+            auxiliar = x.rstrip()
+            dic[auxiliar] = []
+            counter = 0
+        if(x[0] != '*'):
             dic[auxiliar].append(x.rstrip())
 
     for i in dic:
         for j in range(len(dic[i])):
-            dic[i][j]=dic[i][j].split()
+            dic[i][j] = dic[i][j].split()
             for k in range(len(dic[i][j])):
                 try:
-                    dic[i][j][k]=float(dic[i][j][k])
+                    dic[i][j][k] = float(dic[i][j][k])
                 except ValueError:
-                    p=0;
+                    p = 0
 
-def calcMRigidez(e,a,l,pontoA,pontoB):
 
-    alfa= m.atan2(pontoB[1]-pontoA[1],pontoB[0]-pontoA[0])
-    c=m.cos(alfa)
-    s=m.sin(alfa)
+def calcMRigidez(e, a, l, pontoA, pontoB):
 
-    M = np.array([[c**2,c*s,-c**2,-c*s],
-    [c*s,s**2,-c*s,-s**2],
-    [-c**2,-c*s,c**2,c*s],
-    [-c*s,-s**2,c*s,s**2]])
+    alfa = m.atan2(pontoB[1]-pontoA[1], pontoB[0]-pontoA[0])
+    c = m.cos(alfa)
+    s = m.sin(alfa)
 
+    M = np.array([[c**2, c*s, -c**2, -c*s],
+                  [c*s, s**2, -c*s, -s**2],
+                  [-c**2, -c*s, c**2, c*s],
+                  [-c*s, -s**2, c*s, s**2]])
 
     M = M.round(7)
     return ((e*a/l)*M)
 
-def Gauss(u, F, M, tolerancia, loopN):
 
-    uv = np.array([[0.0],[0.0],[0.0]]) ######UV TEM QUE SER DO TAMANHO DE U, PREENCHIDO COM 0
+def Gauss(F, M, tolerancia, loopN):
+
+    u = np.zeros((len(M[0]), 1))
+    uv = np.zeros((len(M[0]), 1))
 
     checkt2 = 0
     loop = 0
 
     while(True and (loop < loopN)):
+
         index = 0
         checkt1 = 0
         for i in range(len(M)):
-            soma=0
+            soma = 0
             for j in range(len(M[i])):
                 soma += M[i][j]*u[j][0]
             uv[i][0] = u[i][0]
-            u[i][0] = ( F[i][0] - soma + u[i][0] * M[i][index] ) / M[i][index]
+            u[i][0] = (F[i][0] - soma + u[i][0] * M[i][index]) / M[i][index]
             index += 1
             if(loop > 0):
                 check = (u[i][0] - uv[i][0]) / u[i][0]
