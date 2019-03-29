8368ee10c7a110cf4c48381005b1ed7a602b3434
LucasVaz97 <lucastv@al.insper.edu.br>
2019-03-29 01:36:32 -0300

    ajustes

--- mec.py
+++ mec.py
@@ -3,46 +3,35 @@ import math as m
 
 
 def readDic(filename):
-
     dic = {}
     f = open(filename, "r")
     auxiliar = ""
-
     for x in f:
         if(x[0] == "*"):
             auxiliar = x.rstrip()
             dic[auxiliar] = []
-
         if(x[0] != '*'):
             dic[auxiliar].append(x.rstrip())
-
     for i in dic:
         for j in range(len(dic[i])):
             dic[i][j] = dic[i][j].split()
-
             for k in range(len(dic[i][j])):
                 try:
                     dic[i][j][k] = float(dic[i][j][k])
-
                 except ValueError:
                     p = 0
-
     return dic
 
 
 def calcMRigidez(e, a, l, pontoA, pontoB):
-
     alfa = m.atan2(pontoB[1]-pontoA[1], pontoB[0]-pontoA[0])
     c = m.cos(alfa)
     s = m.sin(alfa)
-
     M = np.array([[c**2, c*s, -c**2, -c*s],
                   [c*s, s**2, -c*s, -s**2],
                   [-c**2, -c*s, c**2, c*s],
                   [-c*s, -s**2, c*s, s**2]])
-
     M = M.round(7)
-
     return ((e*a/l)*M)
 
 
@@ -85,75 +74,123 @@ def calcGauss(F, M, tolerancia, loopN):
 
     return u
 
-def dicCalcMRigid(dic):
+def makePointList(coordinates, incidences):
+
+    point_list = []
+    
+    for i in range(len(incidences)):
+
+        indexA = int(incidences[i][1]) 
+        indexB = int(incidences[i][2])
+
+        incidence_list = []
+
+        pointA = [coordinates[indexA][1], coordinates[indexA][2]]
+        incidence_list.append(pointA)
+        pointB = [coordinates[indexB][1], coordinates[indexB][2]]
+        incidence_list.append(pointB)
+
+        point_list.append(incidence_list)
+
+    return point_list
+
+def makeRigidMatrixList(point_list, incidences, materials, geometric): ## [[[1,1][2,2]][[3,3][4,4]]]
 
     listM = []
-    coordinates = dic["*COORDINATES"]
-    materials = dic["*MATERIALS"]
-    geometric = dic["*GEOMETRIC_PROPERTIES"]
     
-    for i in range(len(dic["*INCIDENCES"])):
+    for i in range(len(incidences)):
 
-        indexA = int(dic["*INCIDENCES"][i][1]) 
-        indexB = int(dic["*INCIDENCES"][i][2])
-
-        pontoA = [coordinates[indexA][1], coordinates[indexA][2]]
-        pontoB = [coordinates[indexB][1], coordinates[indexB][2]]
+        pointA = point_list[i][0]
+        pointB = point_list[i][1]
 
         e = materials[i+1][0]
         a = geometric[i+1][0]
-        l = m.hypot(pontoB[0] - pontoA[0], pontoB[1] - pontoA[1])
+        l = m.hypot(pointB[0] - pointA[0], pointB[1] - pointA[1])
         
-        listM.append(calcMRigidez(e, a, l, pontoA, pontoB))
+        listM.append(calcMRigidez(e, a, l, pointA, pointB))
 
     return listM
 
-def finalU(u): #preenche a matriz u corretamente no formato u = [1, 2, 3, 4]
+def calcStrainsNStresses(point_list, incidences, materials, u):
+    
+    strains = []
+    stresses = []
+
+    for i in range(len(incidences)):
+
+        up = []
+
+        pointA = point_list[i][0]
+        pointB = point_list[i][1]
+
+        e = materials[i+1][0]
+        l = m.hypot(pointB[0] - pointA[0], pointB[1] - pointA[1])
+        
+        indexA = int(incidences[i][1]) - 1
+        indexB = int(incidences[i][2]) - 1
+
+        up.append(u[indexA][0])
+        up.append(u[indexA][1])
+        up.append(u[indexB][0])
+        up.append(u[indexB][1])
+
+        x = calcProdutoEscalar(pointA, pointB, up) ####
+
+        strains.append(x/l)
+        stresses.append(x*e/l)
+
+    return strains, stresses
+
+def finalU(u): #preenche a matriz u corretamente no formato u = [[1, 2], [3, 4]]
     pass
 
+def calcProdutoEscalar(pointA, pointB, up): #Calcula o produto escalar de 2 pontos com a matriz de c e s
+    return 1
 
-def makeExitFile(filename, u, strains, stresses):
-
+def makeExitFile(filename, u, strains, stresses, forces):
+    
     file = open(filename,"w")
-
     file.write("*DISPLACEMENTS\n")
     j = 1
-
-    for i in range(0, len(u)-1, 2):
-        file.write(str(j) + " ")
-        file.write(str(u[i]) + " ")
-        file.write(str(u[i+1]) + "\n")
+    for i in range(len(u)):
+        file.write(str(j) + " " + str(u[i][0]) + " " + str(u[i][1]) + "\n")
         j += 1
-
-    file.write("\n")
-    file.write("*ELEMENT_STRAINS\n")
+    file.write("\n"+"*ELEMENT_STRAINS\n")
     j = 1
     for i in range(len(strains)):
-        file.write(str(j) + " ")
-        file.write(str(strains[i]) + "\n")
+        file.write(str(j) + " " + str(strains[i]) + "\n")
         j += 1
-
-    file.write("\n")
-    file.write("*ELEMENT_STRESSES\n")
+    file.write("\n"+"*ELEMENT_STRESSES\n")
     j = 1
     for i in range(len(stresses)):
-        file.write(str(j) + " ")
-        file.write(str(stresses[i]) + "\n")
+        file.write(str(j) + " " + str(stresses[i]) + "\n")
         j += 1
+    file.write("\n"+"*REACTION_FORCES\n")
 
-    file.write("\n")
-    file.write("*REACTION_FORCES\n")
+def main():
 
-M = np.array([[1.59e8,-0.40e8,-0.54e8],
-              [-0.40e8,1.70e8,0.40e8],
-              [-0.54e8,0.40e8,0.54e8],])
+    #M = np.array([[1.59e8,-0.40e8,-0.54e8],
+    #            [-0.40e8,1.70e8,0.40e8],
+    #            [-0.54e8,0.40e8,0.54e8],])
 
-F=np.array([[0.0],[150.0],[-100]])
+    #F=np.array([[0.0],[150.0],[-100]])
 
-u = calcGauss(F, M, 0.005, 10)+[10]
+    #u = calcGauss(F, M, 0.005, 10)+[10]
+    
+    dic = readDic("data.txt")
+    coordinates = dic["*COORDINATES"]
+    incidences = dic["*INCIDENCES"]
+    geometric = dic["*GEOMETRIC_PROPERTIES"]
+    materials = dic["*MATERIALS"]
 
-ut = [1, 2, 3, 4]
-strains = [1, 2, 3]
-stresses = [1, 2, 3]
+    point_list = makePointList(coordinates, incidences)
 
-makeExitFile("saida.txt", ut, strains, stresses)
+    ut = [[1, 2],[3, 4],[5,6]]
+    calcsns = calcStrainsNStresses(point_list, incidences, materials, ut)
+    strains = calcsns[0]
+    stresses = calcsns[1]
+    forces = [1]
+
+    makeExitFile("saida.txt", ut, strains, stresses, forces)
+
+main()
